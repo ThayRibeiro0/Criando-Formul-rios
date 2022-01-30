dba09be84c10a1a4112af7403332b20111fead66
matheus correa <mathec@live.com>
2022-01-30 00:55:34 -0300

    fix: database connection and add docker

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 //Conexao com o banco de dados Mysql
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postapp', 'root', 'Usbw@2951', {
-    host: "10.0.0.108",
+const sequelize = new Sequelize('postapp', 'root', 'usbw2951', {
+    host: "127.0.0.1",
     dialect: 'mysql'
     })
 
